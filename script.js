0029c660182290eacc74d44231f2fb397af44108
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2025-11-06 08:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstWord(s) {
   // your code here
 	let reg=/\s/;
-	let arr=s.split(reg);
+	let arr=s.trim().split(reg);
 	return arr[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
